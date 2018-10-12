38b44f057b54d64076cf575c89b1f4dc2a538995
gAltelino <galtelino@gmail.com>
2018-10-12 17:30:35 -0300

    Finished 1

---
+++ FunctionDelegateExample/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionDelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Student() { ID = 0, Name = "Test" };
+            var students = new List<Student>(){
+                new Student{ID = 1, Name="Tim"},
+                new Student{ID = 2, Name="Ella"},
+                new Student{ID = 3, Name="Tom"},
+                new Student{ID = 4, Name="Roman"}
+            };
+
+            Func<Student, string> func1 = new Func<Student, string>(GetStudentInfo);
+
+            var info0 = func1(test);
+            System.Console.WriteLine(info0);
+
+            Func<Student, string> func2 = s => $"ID:{s.ID} Name:{s.Name}";
+
+            var info1 = func2(test);
+            System.Console.WriteLine(info1);
+
+            var info2 = Select(students, s => $"ID:{s.ID} Name:{s.Name}");
+
+            foreach (var info in info2)
+            {
+                System.Console.WriteLine(info);
+            }
+
+            var info3 = students.Select(s => $"ID:{s.ID} Name:{s.Name}");
+
+            foreach (var info in info3)
+            {
+                System.Console.WriteLine(info);
+            }
+
+        }
+
+        static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> sources, Func<TSource, TResult> func)
+        {
+            var result = new List<TResult>();
+            foreach (var item in sources)
+            {
+                result.Add(func(item));
+            }
+            return result;
+        }
+
+        static string GetStudentInfo(Student stud)
+        {
+            var info = $"ID:{stud.ID} Name:{stud.Name}";
+            return info;
+        }
+    }
+
+    public static class MyExtension
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> sources, Func<TSource, TResult> func)
+        {
+            var result = new List<TResult>();
+            foreach (var item in sources)
+            {
+                result.Add(func(item));
+            }
+            return result;
+        }
+    }
+
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LinqApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using LinqApp2;
+
+namespace LinqApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var record = DataLoader.Load(@"/home/altrunox/Documents/DEV286x/LinqApp2");
+            var female10 = record.Where(r => r.Gender == Gender.Female && r.Rank <= 10);
+            var male10 = record.Where(m => m.Gender == Gender.Male && m.Rank <= 10);
+            var maleAnotherWay = from r in record where r.Gender == Gender.Male && r.Rank <= 10 select r;
+
+            foreach (var r in male10)
+            {
+                System.Console.WriteLine(r);
+            }
+
+            foreach (var r in female10)
+            {
+                System.Console.WriteLine(r);
+            }
+
+            foreach (var r in maleAnotherWay)
+            {
+                System.Console.WriteLine(r);
+            }
+
+
+        }
+    }
+}
